1add8f48b72ba5b6b10ce436d9d8f95f48210792
Yerkeshhhh <117143038+Yerkeshhhh@users.noreply.github.com>
2023-04-07 16:14:37 +0600

    did some changes

--- ads/src/Problem1.java
+++ ads/src/Problem1.java
@@ -1,9 +1,9 @@
 public class Problem1 {
-    public static int findMin(int[] arr, int n) {
+    public static int findMinimum(int[] arr, int n) {
         if (n == 1) {
             return arr[0];
         }
-        int min = findMin(arr, n - 1);
+        int min = findMinimum(arr, n - 1);
         if (arr[n - 1] < min) {
             min = arr[n - 1];
         }
@@ -11,8 +11,8 @@ public class Problem1 {
     }
     public static void main(String[] args) {
         int[] arr = {10, 1, 32, 3, 45};
-        int n = arr.length;
-        int min = findMin(arr, n);
+        int n = 5;
+        int min = findMinimum(arr, n);
         System.out.println(min);
     }
 }
