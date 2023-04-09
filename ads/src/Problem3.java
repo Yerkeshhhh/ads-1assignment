373424160a160636552b6eefeaf4ecc636b50ebb
Yerkeshhhh <117143038+Yerkeshhhh@users.noreply.github.com>
2023-04-09 14:10:02 +0600

    did some changes

--- ads/src/Problem3.java
+++ ads/src/Problem3.java
@@ -1,25 +1,42 @@
 import java.util.Scanner;
 
+/*
+isPrimeHelper - this function is helper function to @isPrime to check if the integer n is prime
+isPrime - checks if the n is prime
+n - inputted integer
+d - divisor to check n is prime
+ */
+
+
 public class Problem3 {
-    public static void main(String[] args) {
-        System.out.println(Helper(7, 2));
-    }
-
-    public static boolean Helper(int i, int n) {
-        if (i<=1)
-            return true;
-        if (n%i==0)
+    public static boolean isPrime(int n) {
+        if (n==1 && n==0) {
             return false;
-        else
-            return Helper(n, i-1);
-    }
-
-    public static boolean isPrime(int a,int i){
-        if(i <= 1){
+        } else if (n==2) {
             return true;
-        } else if (a%i == 0) {
+        } else if (n%2==0) {
             return false;
         }
-        return isPrime(a,i-1);
+        return isPrimeHelper(n,3);
+    }
+
+    public static boolean isPrimeHelper(int n, int d) {
+        if (d>Math.sqrt(n)) {
+            return true;
+        } else if (n%d==0) {
+            return false;
+        }
+        return isPrimeHelper(n, d+2);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        boolean prime = isPrime(n);
+        if (prime) {
+            System.out.println("Prmie");
+        } else {
+            System.out.println("Composite");
+        }
     }
 }
